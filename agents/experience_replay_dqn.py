eed6cb3a0e2b7c0801531585d5be5e0634494a53
sunaz <suzanhadzhieva@gmail.com>
2021-01-23 16:06:45 +0200

    Create DQNAgent class

--- agents/experience_replay_dqn.py
+++ agents/experience_replay_dqn.py
@@ -1,140 +1,17 @@
 from utils.memory import Memory
-from keras.optimizers import Adam
-from keras.models import Sequential
-from keras.layers import Dense, Dropout
-import pandas as pd
-from operator import add
 from random import randint
+from agents.dqn_agent import DQNAgent
 from keras.utils import to_categorical
 from game import Game
 from game_initializer import *
 from utils.settings import game_settings
 
 
-class ExperienceReplayDQNAgent(object):
+class ExperienceReplayDQNAgent(DQNAgent):
 
     def __init__(self):
-        self.reward = 0
-        self.gamma = 0.9
-        self.dataframe = pd.DataFrame()
-        self.short_memory = np.array([])
-        self.agent_target = 1
-        self.agent_predict = 0
-        self.learning_rate = 0.0005
-        self.model = self.network()
-        # self.model = self.network("weights.hdf5")
-        self.epsilon = 0
-        self.actual = []
+        super().__init__()
         self.memory = Memory(1000)
-
-    def get_state(self, game, player, food):
-
-        state = [
-            (player.x_change == 20 and player.y_change == 0 and (
-                    (list(map(add, player.position[-1], [20, 0])) in player.position) or
-                    player.position[-1][0] + 20 >= (game.game_width - 20) or
-                    (list(map(add, player.position[-1], [20, 0])) in game.barrierPositions)
-            )) or
-            (player.x_change == -20 and player.y_change == 0 and (
-                    (list(map(add, player.position[-1], [-20, 0])) in player.position) or
-                    player.position[-1][0] - 20 < 20 or
-                    (list(map(add, player.position[-1], [-20, 0])) in game.barrierPositions)
-            )) or
-            (player.x_change == 0 and player.y_change == -20 and (
-                    (list(map(add, player.position[-1], [0, -20])) in player.position) or
-                    player.position[-1][-1] - 20 < 20 or
-                    (list(map(add, player.position[-1], [0, -20])) in game.barrierPositions)
-            )) or
-            (player.x_change == 0 and player.y_change == 20 and (
-                    (list(map(add, player.position[-1], [0, 20])) in player.position) or
-                    player.position[-1][-1] + 20 >= (game.game_height-20) or
-                    (list(map(add, player.position[-1], [0, 20])) in game.barrierPositions)
-            )),  # danger straight
-
-            (player.x_change == 0 and player.y_change == -20 and (
-                    (list(map(add,player.position[-1],[20, 0])) in player.position) or
-                    player.position[ -1][0] + 20 > (game.game_width-20) or
-                    (list(map(add, player.position[-1], [20, 0])) in game.barrierPositions)
-            )) or
-            (player.x_change == 0 and player.y_change == 20 and (
-                    (list(map(add,player.position[-1], [-20,0])) in player.position) or
-                    player.position[-1][0] - 20 < 20 or
-                    (list(map(add, player.position[-1], [-20, 0])) in game.barrierPositions)
-            )) or
-            (player.x_change == -20 and player.y_change == 0 and (
-                    (list(map(add ,player.position[-1],[0,-20])) in player.position) or
-                    player.position[-1][-1] - 20 < 20 or
-                    (list(map(add, player.position[-1], [0, -20])) in game.barrierPositions)
-            )) or
-            (player.x_change == 20 and player.y_change == 0 and (
-                    (list(map(add,player.position[-1],[0,20])) in player.position) or
-                    player.position[-1][-1] + 20 >= (game.game_height-20) or
-                    (list(map(add, player.position[-1], [-20, 0])) in game.barrierPositions)
-            )),  # danger right
-
-            (player.x_change == 0 and player.y_change == 20 and (
-                    (list(map(add,player.position[-1],[20,0])) in player.position) or
-                    player.position[-1][0] + 20 > (game.game_width-20) or
-                    (list(map(add, player.position[-1], [20, 0])) in game.barrierPositions)
-            )) or
-            (player.x_change == 0 and player.y_change == -20 and (
-                    (list(map(add, player.position[-1],[-20,0])) in player.position) or
-                    player.position[-1][0] - 20 < 20 or
-                    (list(map(add, player.position[-1], [-20, 0])) in game.barrierPositions)
-            )) or
-            (player.x_change == 20 and player.y_change == 0 and (
-                    (list(map(add,player.position[-1],[0,-20])) in player.position) or
-                    player.position[-1][-1] - 20 < 20 or
-                    (list(map(add, player.position[-1], [0, -20])) in game.barrierPositions)
-            )) or
-            (player.x_change == -20 and player.y_change == 0 and (
-                    (list(map(add,player.position[-1],[0,20])) in player.position) or
-                    player.position[-1][-1] + 20 >= (game.game_height-20) or
-                    (list(map(add, player.position[-1], [0, 20])) in game.barrierPositions)
-            )), #danger left
-
-            player.x_change == -20,  # move left
-            player.x_change == 20,  # move right
-            player.y_change == -20,  # move up
-            player.y_change == 20,  # move down
-            food.x_food < player.x,  # food left
-            food.x_food > player.x,  # food right
-            food.y_food < player.y,  # food up
-            food.y_food > player.y  # food down
-        ]
-
-        for i in range(len(state)):
-            if state[i]:
-                state[i] = 1
-            else:
-                state[i] = 0
-
-        return np.asarray(state)
-
-    def set_reward(self, player, crash):
-        self.reward = 0
-        if crash:
-            self.reward = -10
-            return self.reward
-        if player.eaten:
-            self.reward = 10
-        return self.reward
-
-    def network(self, weights=None):
-        model = Sequential()
-        model.add(Dense(120, activation='relu', input_dim=11))
-        model.add(Dropout(0.15))
-        model.add(Dense(120, activation='relu'))
-        model.add(Dropout(0.15))
-        model.add(Dense(120, activation='relu'))
-        model.add(Dropout(0.15))
-        model.add(Dense(3, activation='softmax'))
-        opt = Adam(self.learning_rate)
-        model.compile(loss='mse', optimizer=opt)
-
-        if weights:
-            model.load_weights(weights)
-        return model
 
     def remember(self, state, action, reward, next_state, done):
         self.memory.store((state, action, reward, next_state, done))
@@ -199,7 +76,7 @@ class ExperienceReplayDQNAgent(object):
                 player1.do_move(final_move, player1.x, player1.y, game, food1, self)
                 state_new = self.get_state(game, player1, food1)
 
-                # set treward for the new state
+                # set reward for the new state
                 reward = self.set_reward(player1, game.crash)
 
                 # train short memory base on the new action and state
